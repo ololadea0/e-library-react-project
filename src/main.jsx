5f9f38c328d3b299a99016aed36c0c03d3dd5036
ololadea0 <opeyemifawaz11@gmail.com>
2025-11-22 12:26:31 +0100

    tried fixing routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/e-library-react-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
