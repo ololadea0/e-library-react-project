447c701797af050d379d33e6e2b61b6200d14d36
ololadea0 <opeyemifawaz11@gmail.com>
2025-11-22 13:17:08 +0100

    fdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./css/fonts.css";
 import "./css/style.css";
 import { useEffect } from "react";
@@ -50,21 +50,21 @@ function App() {
       <Navbar />
 
       {/* Routes */}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/books/:id" element={<BookDetails />} />
-
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/admin/createbook" element={<CreateBook />} />
-        <Route path="/admin/edit" element={<Edit />} />
-        <Route path="/admin/edit/:id" element={<EditBook />} />
-        <Route path="/admin/deletebook" element={<DeleteBook />} />
-        <Route path="/library/category/:name" element={<CategoryPage />} />
-      </Routes>
+      <Router basename="/e-library-react-project">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/admin/createbook" element={<CreateBook />} />
+          <Route path="/admin/edit" element={<Edit />} />
+          <Route path="/admin/edit/:id" element={<EditBook />} />
+          <Route path="/admin/deletebook" element={<DeleteBook />} />
+          <Route path="/library/category/:name" element={<CategoryPage />} />
+        </Routes>
+      </Router>
 
       {/* Footer */}
       <Footer />
