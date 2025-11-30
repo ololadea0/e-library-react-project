ddb90aa1792762c7dbbd072d4dfc423070a90389
ololadea0 <opeyemifawaz11@gmail.com>
2025-11-30 15:05:13 +0100

    redefined the files

--- src/hooks/Uploader.jsx
+++ src/hooks/Uploader.jsx
@@ -1,4 +1,4 @@
-import { supabase } from "../components/supabaseClient";
+import { supabase } from "./supabaseClient";
 
 export const UploadPdf = async (file) => {
   try {
--- src/hooks/UseFetchSupabase.jsx
+++ src/hooks/UseFetchSupabase.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { supabase } from "./components/supabaseClient";
+import { supabase } from "./supabaseClient";
 
 const useFetchSupabase = (tableName) => {
   const [data, setData] = useState(null);
